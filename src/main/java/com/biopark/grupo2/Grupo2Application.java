cad91a360e5ebe0a892abd1faa3a883b3422ba2e
Cesar Augusto da Silva Pisa <cesar.pisa@alunos.bpkedu.com.br>
2024-04-03 20:24:13 -0300

    entitiesDAO

--- src/main/java/com/biopark/grupo2/Grupo2Application.java
+++ src/main/java/com/biopark/grupo2/Grupo2Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Grupo2Application {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(Grupo2Application.class, args);
 	}
 
