e4704a64f2140464224f32c7ca6ae628f911082c
heloshartmann <133721438+heloshartmann@users.noreply.github.com>
2024-04-04 11:24:12 -0300

    entidade formulario

---
+++ src/main/java/com/biopark/grupo2/model/Formulario.java
@@ -0,0 +1,44 @@
+package com.biopark.grupo2.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.Objects;
+
+@Entity
+public class Formulario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idFormulario;
+    private String titulo;
+
+    public int getIdFormulario() {
+        return idFormulario;
+    }
+
+    public void setIdFormulario(int idFormulario) {
+        this.idFormulario = idFormulario;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Formulario that)) return false;
+        return idFormulario == that.idFormulario;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idFormulario);
+    }
+}
