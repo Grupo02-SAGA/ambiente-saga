4f8fa1c6f31f1df104ddcc26271d0e702a6e0c0f
Gustavo Dechotti <75210025+GusDechotti@users.noreply.github.com>
2024-04-12 21:58:01 -0300

    começo back

--- src/main/java/com/biopark/grupo2/controller/Grupo2Controller.java
+++ src/main/java/com/biopark/grupo2/controller/Grupo2Controller.java
@@ -1,4 +1,30 @@
 package com.biopark.grupo2.controller;
 
+import com.biopark.grupo2.model.Formulario;
+import com.biopark.grupo2.model.Pergunta;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.ui.Model;
+
+@Controller
 public class Grupo2Controller {
+@GetMapping("/")
+    public ModelAndView grupo2(){
+    ModelAndView modelAndView = new ModelAndView();
+    modelAndView.setViewName("criar+");
+    return modelAndView;
+}
+@PostMapping("/criar+")
+public String criarMais(@ModelAttribute Formulario formulario,
+                        @ModelAttribute Pergunta pergunta,
+                        Model model){
+    
+    model.addAttribute("result",result);
+    return "criar+";
+}
+
 }
