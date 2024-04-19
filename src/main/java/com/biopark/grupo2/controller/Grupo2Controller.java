8ddead2158c6a4475200b52ef88ce4aea307c7ea
devPisa <cesar.au563@gmail.com>
2024-04-19 08:59:35 -0300

    Camada de servidor pronta para integração

--- src/main/java/com/biopark/grupo2/controller/Grupo2Controller.java
+++ src/main/java/com/biopark/grupo2/controller/Grupo2Controller.java
@@ -1,7 +1,33 @@
 package com.biopark.grupo2.controller;
 
-import org.springframework.stereotype.Controller;
+import com.biopark.grupo2.model.Empresa;
+import com.biopark.grupo2.model.ResquestEmpresas;
+import com.biopark.grupo2.repository.RepositoryEmpresa;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+@RestController
+@RequestMapping("/novaEmpresa")
 public class Grupo2Controller {
+
+    @Autowired
+    private RepositoryEmpresa empresa;
+
+    @GetMapping("/")
+    public ModelAndView showForm() {
+       ModelAndView modelAndView = new ModelAndView();
+       modelAndView.setViewName("novaEmpresa");
+       return  modelAndView;
+    }
+
+    @CrossOrigin(origins = "http://localhost:8080")
+    @PostMapping
+    public ResponseEntity createEmpresa(@RequestBody @Valid ResquestEmpresas requestEmpresas){
+        Empresa newEmpresa = new Empresa(requestEmpresas);
+        empresa.save(newEmpresa);
+        return  ResponseEntity.ok().build();
+    }
 }
