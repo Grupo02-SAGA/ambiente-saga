6a5ee07bf8ace9db3466ce72015d52ab1aed3909
Gustavo Dechotti <75210025+GusDechotti@users.noreply.github.com>
2024-04-19 18:34:43 -0300

    Consultinhas no bando :3

--- src/main/java/com/biopark/grupo2/controller/Grupo2Controller.java
+++ src/main/java/com/biopark/grupo2/controller/Grupo2Controller.java
@@ -1,38 +1,30 @@
 package com.biopark.grupo2.controller;
 
 import com.biopark.grupo2.model.Formulario;
-import com.biopark.grupo2.model.Pergunta;
+import com.biopark.grupo2.repository.RepositoryFormulario;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Optional;
 
 @Controller
 public class Grupo2Controller {
-    @GetMapping("/")
-    public ModelAndView grupo2() {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("criarMais");
-        return modelAndView;
+
+
+    @Autowired
+    private RepositoryFormulario repositoryFormulario;
+
+    public String getTituloFormularioPorId(int id_formulario) {
+        Optional<Formulario> formulario = repositoryFormulario.findByid_formulario(id_formulario);
+        return formulario.isPresent() ? formulario.get().getTitulo() : "Formulário não encontrado";
     }
 
-    @PostMapping("/imprimeTitulo")
-    public String criarMaisTitulo(@ModelAttribute Formulario formulario,
-                            @ModelAttribute Pergunta pergunta,
-                            Model model) {
-        model.addAttribute("titulo", formulario.getTitulo());
-        return "criarMais";
-    }
-    @PostMapping("/imprimePergunta")
-    public String criarMaisPergunta(@ModelAttribute Formulario formulario,
-                                    @ModelAttribute Pergunta pergunta,
-                                    Model model){
-        //pegar os que tem fk do id fornecido
-        while (.next){
-            model.addAttribute("tituloPergunta",.titulo())
-        }
+    @GetMapping("/")
+    public String grupo2(Model model) {
+        model.addAttribute("titulo", getTituloFormularioPorId(1));
         return "criarMais";
     }
 }
+
--- src/main/java/com/biopark/grupo2/repository/RepositoryFormulario.java
+++ src/main/java/com/biopark/grupo2/repository/RepositoryFormulario.java
@@ -3,5 +3,8 @@ package com.biopark.grupo2.repository;
 import com.biopark.grupo2.model.Formulario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RepositoryFormulario extends JpaRepository<Formulario, Long> {
+    Optional<Formulario> findByid_formulario(int id_formulario);
 }
