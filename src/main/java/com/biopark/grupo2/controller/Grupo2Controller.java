20127196d129c326fce2d8ae463c81dd1a9de0b2
devPisa <cesar.au563@gmail.com>
2024-04-20 02:07:09 -0300

    Ajuste camada de controller

--- src/main/java/com/biopark/grupo2/controller/Grupo2Controller.java
+++ src/main/java/com/biopark/grupo2/controller/Grupo2Controller.java
@@ -1,7 +1,38 @@
 package com.biopark.grupo2.controller;
 
-import org.springframework.stereotype.Controller;
+import com.biopark.grupo2.model.Empresa;
+import com.biopark.grupo2.model.ResquestEmpresa;
+import com.biopark.grupo2.repository.RepositoryEmpresa;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+@RestController
+@RequestMapping("/novaEmpresa")
 public class Grupo2Controller {
+
+    @Autowired
+    private RepositoryEmpresa repositoryEmpresa;
+
+    @GetMapping("/")
+    public ModelAndView showForm() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("novaEmpresa");
+        return  modelAndView;
+    }
+    @GetMapping("/mostrarForm")
+    public ResponseEntity getAllEmpresas(){
+        var allEmpresas = repositoryEmpresa.findAll();
+        return ResponseEntity.ok(allEmpresas);
+    }
+
+    @CrossOrigin(origins = "http://localhost:8080")
+    @PostMapping
+    public ResponseEntity createEmpresa(@RequestBody @Valid ResquestEmpresa resquestEmpresa){
+        Empresa newEmpresa = new Empresa(resquestEmpresa);
+        repositoryEmpresa.save(newEmpresa);
+        return  ResponseEntity.ok().build();
+    }
 }
