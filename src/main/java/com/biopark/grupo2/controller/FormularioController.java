986ac24b38e030ee567806cd53249bdb1eb23740
Kainak <natanfkainak@gmail.com>
2024-04-25 02:52:47 -0300

    Ajustes no CSS

--- src/main/java/com/biopark/grupo2/controller/FormularioController.java
+++ src/main/java/com/biopark/grupo2/controller/FormularioController.java
@@ -3,6 +3,7 @@ import com.biopark.grupo2.model.Formulario;
 import com.biopark.grupo2.repository.RepositoryFormulario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import java.util.List;
@@ -25,15 +26,16 @@ public class FormularioController {
         return repositoryFormulario.findAll();
     }
 
-//    @GetMapping(value = "/{id}")
-//    public Formulario findById(@PathVariable Long id){
-//        Formulario result = repositoryFormulario.findById(id).get();
-//        return result;
-//    }
-//
-//    @PostMapping
-//    public Formulario insert(@RequestBody Formulario formulario) {
-//        Formulario result = repositoryFormulario.save(formulario);
-//        return result;
-//    }
+
+    @GetMapping(value = "/{id}")
+    public Formulario findById(@PathVariable Long id){
+        Formulario result = repositoryFormulario.findById(id).get();
+        return result;
+    }
+
+    @PostMapping
+    public Formulario insert(@RequestBody Formulario formulario) {
+        Formulario result = repositoryFormulario.save(formulario);
+        return result;
+    }
 }
--- src/main/java/com/biopark/grupo2/repository/RepositoryFormulario.java
+++ src/main/java/com/biopark/grupo2/repository/RepositoryFormulario.java
@@ -1,6 +1,10 @@
 package com.biopark.grupo2.repository;
 import com.biopark.grupo2.model.Formulario;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
 public interface RepositoryFormulario extends JpaRepository<Formulario, Long> {
 }
+
 
