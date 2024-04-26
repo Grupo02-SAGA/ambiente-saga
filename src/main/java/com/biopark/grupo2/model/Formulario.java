1bbdefc4fdfc5e489cf4d78062fce587559cf3fa
Kainak <125594835+Kainak@users.noreply.github.com>
2024-04-26 14:05:08 -0300

    6 visualizao de formulriosbase desenvolvimento (#53)
    *Visualizao de formulrios base desenvolvimento

--- src/main/java/com/biopark/grupo2/model/Formulario.java
+++ src/main/java/com/biopark/grupo2/model/Formulario.java
@@ -1,57 +1,26 @@
 package com.biopark.grupo2.model;
-
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.Objects;
 
-@Entity
+@Entity(name = "formulario")
+@Table(name ="formulario")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id_formulario")
+
 public class Formulario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_formulario")
     private int id_formulario;
+
+    @Column(name = "titulo")
     private String titulo;
-    private boolean base;
 
-    public Formulario() {
-    }
-    public Formulario(int id_formulario, String titulo) {
-        this.id_formulario = id_formulario;
-        this.titulo = titulo;
-    }
-
-    public int getId_formulario() {
-        return id_formulario;
-    }
-
-    public void setId_formulario(int idFormulario) {
-        this.id_formulario = idFormulario;
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public boolean isBase() {
-        return base;
-    }
-
-    public void setBase(boolean base) {
-        this.base = base;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Formulario that)) return false;
-        return id_formulario == that.id_formulario;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id_formulario);
-    }
+    @Column(name="base")
+    private Boolean base;
 }
---
+++ src/main/resources/static/scripts/buscaFormularioBase.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    const searchInput = document.getElementById('searchInput');//pega o que o usuário digitar
+    const tableRows = document.querySelectorAll('.listaForm')
+    //toda vez que digitar dispara isso
+    searchInput.addEventListener('input', function() {
+
+        const searchText = this.value.toLowerCase();//pega a
+
+        tableRows.forEach(function(row) {
+            const rowData = row.innerText.toLowerCase(); // Pega o innerText de cada linha
+            console.log(rowData);
+
+            if (rowData.includes(searchText)) {
+                row.style.display = '';
+            } else {
+                row.style.display = 'none';
+            }
+        });
+    });
+});
