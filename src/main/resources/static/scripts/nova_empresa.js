8e9c5054f315fd6cd41e72997b2d8d26d97dfdb0
devPisa <cesar.au563@gmail.com>
2024-04-27 21:59:20 -0300

    Implementação de fragmentação e popup concluida

--- src/main/resources/static/scripts/nova_empresa.js
+++ src/main/resources/static/scripts/nova_empresa.js
@@ -1,38 +1,29 @@
-const carregar_popup_cancelar = () => {
-    const cancelar_popup = document.getElementById("fragmento_popup_cancelar");
+const button_cancel_form = document.getElementById("cancel_form");
+const fade_fundo = document.getElementById("fade_cancelar");
+const popup = document.getElementById("popup");
+const button_sim_popup = document.getElementById("confirmar_fragmento");
+const button_nao_popup = document.getElementById("cancelar_fragmento");
 
-    const css_dinamico = document.createElement('link');
-    css_dinamico.rel = 'stylesheet';
-    css_dinamico.href = '/styles/style_popup_cancelar.css';
-    document.head.appendChild(css_dinamico);
-
-    fetch("/register")
-        .then(response => response.text())
-        .then(html => {
-            cancelar_popup.innerHTML = html;
-
-            const button_sim_popup = document.getElementById("confirmar_fragmento");
-            const button_nao_popup = document.getElementById("cancelar_fragmento");
-            const fade_fundo = document.getElementById("fade");
-
-            const recarregar_pagina = () => {
-                return location.href = "/register";
-            };
-
-            if(button_sim_popup) {
-                button_sim_popup.addEventListener("click", recarregar_pagina);
-            }
-            if(button_nao_popup) {
-                button_nao_popup.addEventListener("click", () => {
-                    cancelar_popup.innerHTML = '';
-                    fade_fundo.style.display = 'none';
-                });
-            }
-        })
-        .catch(error => console.error('Erro ao carregar o fragmento:', error));
+const togglePopup = () => {
+    [popup, fade_fundo].forEach((el) => el.classList.toggle("hide"));
 };
 
-document.addEventListener("DOMContentLoaded", function() {
-    const button_cancelar = document.getElementById("cancelar");
-    button_cancelar.addEventListener("click", carregar_popup_cancelar);
+button_cancel_form.addEventListener("click", () => {
+    togglePopup();
 });
+
+const redirecionar_pagina = () => {
+    return location.href = "/register";
+};
+
+if (button_sim_popup) {
+    button_sim_popup.addEventListener("click", () => {
+        redirecionar_pagina();
+    });
+}
+
+if (button_nao_popup) {
+    button_nao_popup.addEventListener("click", () => {
+        togglePopup();
+    });
+}
--- src/main/resources/static/scripts/popup_confirmacao.js
+++ src/main/resources/static/scripts/popup_confirmacao.js
@@ -1,7 +1,9 @@
-const button_confirmar_popup = document.querySelector("#confirmar_fragmento");
+const button_confirmar_popup = document.getElementById("btn_confirmar_fragmento");
+const fade_fundo_confirmar = document.getElementById("fade_confirmar");
+const body_popup_confirmar = document.getElementById("popup_confirmacao");
 
-const recarregar_pagina = () => {
-    return location.href = "/register"
-}
 
-button_confirmar_popup.addEventListener("click", recarregar_pagina)
+button_confirmar_popup.addEventListener("click", () => {
+    fade_fundo_confirmar.style.display = 'none';
+    body_popup_confirmar.style.display = 'none';
+});
