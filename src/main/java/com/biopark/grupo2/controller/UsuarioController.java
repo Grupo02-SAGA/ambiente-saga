4f260ffa2f766986af2f5a8c54fe0e7768175e01
Matheus de Medeiros <137646466+MedeirosMatheus@users.noreply.github.com>
2024-04-29 14:56:44 -0300

    Ajustes controller

--- src/main/java/com/biopark/grupo2/controller/UsuarioController.java
+++ src/main/java/com/biopark/grupo2/controller/UsuarioController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class UsuarioController {
@@ -14,15 +17,19 @@ public class UsuarioController {
     private RepositoryUsuario repositoryUsuario;
 
     @GetMapping("/novoUsuario")
-    public String showForm(Model model) {
+    public ModelAndView modelAndView(){
         Usuario usuario = new Usuario();
-        model.addAttribute("usuario", usuario);
-        return  "novoUsuario";
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("novoUsuario");
+        modelAndView.addObject("usuario", usuario);
+        return modelAndView;
     }
+
     @PostMapping("/novoUsuario")
-    public String createEmpresa(@ModelAttribute Usuario usuario) {
+    public RedirectView createUsuario(@ModelAttribute ("usuario") Usuario usuario, RedirectAttributes attributes) {
         repositoryUsuario.save(usuario);
-        return "novoUsuario";
+        attributes.addFlashAttribute("condition", "cadastro-ok");
+        return new RedirectView("/novoUsuario");
     }
 
 }
