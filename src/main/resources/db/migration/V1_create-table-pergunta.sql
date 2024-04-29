e09765cd68973e2ddbf7a8517c1b1ac55f8db4db
Gustavo Dechotti <75210025+GusDechotti@users.noreply.github.com>
2024-04-29 19:02:05 -0300

    criado migrations que faltavam

---
+++ src/main/resources/db/migration/V1_create-table-pergunta.sql
@@ -0,0 +1,5 @@
+CREATE TABLE    Pergunta (
+                id_pergunta INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+                titulo VARCHAR(255),
+                documento BOOLEAN NOT NULL
+                )
