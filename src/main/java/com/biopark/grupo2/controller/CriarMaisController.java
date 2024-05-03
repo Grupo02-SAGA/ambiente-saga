a3542419eadac9119cc2bec04c05ef038427f9d6
Gustavo Dechotti <75210025+GusDechotti@users.noreply.github.com>
2024-05-03 18:06:28 -0300

    altera o banco

--- src/main/java/com/biopark/grupo2/controller/CriarMaisController.java
+++ src/main/java/com/biopark/grupo2/controller/CriarMaisController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 import java.util.List;
@@ -28,11 +29,18 @@ public class CriarMaisController {
             Formulario formulario = formularioOpt.get();
             model.addAttribute("titulo", formulario.getTitulo());
             model.addAttribute("perguntas", getQuestionsByFormId(id));
+            model.addAttribute("idFormulario", id);
         }
         return "criarMais";
     }
     public List<String> getQuestionsByFormId(Long id_formulario) {
         return perguntaRepository.findTitlesByFormId(id_formulario);
+    }
+
+    @PostMapping("{idFormulario}/pergunta/{id}/atualizar-ativo")///pergunta/'+ ${pergunta.id_pergunta} + '/atualizar-ativo}
+    public String atualizarAtivo(@PathVariable Long id) {
+        perguntaRepository.atualizarAtivoPorId(id, false);
+        return "redirect:/detalhesDeFormulario/{idFormulario}";
     }
 }
 
