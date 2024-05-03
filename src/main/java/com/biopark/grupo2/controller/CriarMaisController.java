e8ff5d7a14098762d0c1857cdd9c2f9fd80e07aa
devPisa <cesar.au563@gmail.com>
2024-05-03 18:05:31 -0300

    test: Medoto Put no controller

--- src/main/java/com/biopark/grupo2/controller/CriarMaisController.java
+++ src/main/java/com/biopark/grupo2/controller/CriarMaisController.java
@@ -4,12 +4,17 @@ import com.biopark.grupo2.model.Formulario;
 import com.biopark.grupo2.repository.RepositoryFormulario;
 import com.biopark.grupo2.repository.RepositoryPergunta;
 import com.biopark.grupo2.service.FormularioService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.List;
+import java.util.Optional;
 
 
 @Controller
@@ -18,7 +23,7 @@ public class CriarMaisController {
     @Autowired
     private FormularioService formularioService;
 
-   /* @Autowired
+    @Autowired
     private RepositoryFormulario repositoryFormulario;
     @Autowired
     private RepositoryPergunta perguntaRepository;
@@ -30,34 +35,30 @@ public class CriarMaisController {
             Formulario formulario = formularioOpt.get();
             model.addAttribute("titulo", formulario.getTitulo());
             model.addAttribute("perguntas", getQuestionsByFormId(id));
+            model.addAttribute("formulario", formulario);
         }
         return "criarMais";
     }
+
     public List<String> getQuestionsByFormId(Long id_formulario) {
         return perguntaRepository.findTitlesByFormId(id_formulario);
     }
-}*/
 
-    @GetMapping("/alterarFormulario/{id}")
-    public ModelAndView modelAndView( @PathVariable("id") Long id_formulario) {
-        Formulario formulario = formularioService.consultarFormulario(id_formulario);
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("criarMais");
-        modelAndView.addObject("formulario", formulario);
-        return modelAndView;
-    }
-
-    @PostMapping("/alterarFormulario/{id}")
-    public RedirectView atualizarFormulario(@PathVariable Long id, @RequestBody Formulario formulario, RedirectAttributes attributes) {
-        Formulario atualizarFormulario = formularioService.alterarFormulario(id, formulario);
+    @PutMapping("/detalhesDeFormulario/{id}")
+    public RedirectView atualizarFormulario(@ModelAttribute("formulario") Formulario formulario, @PathVariable Long id, RedirectAttributes attributes){
+        Formulario atualizarFormulario = repositoryFormulario.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Formulario não encontrado com o ID: " + id));
         if (atualizarFormulario != null) {
+            BeanUtils.copyProperties(formulario, atualizarFormulario, "id_formulario");
+            repositoryFormulario.save(atualizarFormulario);
             attributes.addFlashAttribute("condition", "cadastro-ok");
-            return  new RedirectView ("/alterarFormulario/" + formulario.getId_formulario());
+            return new RedirectView("/detalhesDeFormulario/" + id);
         } else {
-            return null;
+            return new RedirectView("/popup-de-erro"); /* vai ser implementado ainda*/
         }
     }
 }
+
 
 
 
