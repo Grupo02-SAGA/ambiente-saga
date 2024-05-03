ef667087db2279fdb9d96364c58fc211c334146f
Cesar Augusto da Silva Pisa <cesar.pisa@alunos.bpkedu.com.br>
2024-05-02 22:10:45 -0300

    ImFeat: Implementação front-end editar

--- src/main/resources/static/scripts/btn_ativar_desativar.js
+++ src/main/resources/static/scripts/btn_ativar_desativar.js
@@ -1,8 +1,12 @@
 const estilo = document.getElementById("ativo_inativo");
+const estadoInput = document.getElementById("estadoInput")
 
 const animarEstilo = () => {
     estilo.classList.toggle("inativo");
-    if (estilo.classList.contains("inativo")) {
+
+    estadoInput.value = estadoInput.value === "true" ? "false" : "true";
+
+    if (estilo.classList.contains("inativo") && estadoInput.value === "false") {
         estilo.innerHTML = "Inativo";
     } else {
         estilo.innerHTML = "Ativo";
