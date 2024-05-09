53e196910288142f5d0d0c4d34662f35687d597a
Gustavo Dechotti <75210025+GusDechotti@users.noreply.github.com>
2024-05-08 21:36:04 -0300

    Funcionando criar
    Deve ser arrumada a página

--- src/main/java/com/biopark/grupo2/controller/FormularioController.java
+++ src/main/java/com/biopark/grupo2/controller/FormularioController.java
@@ -1,11 +1,15 @@
 package com.biopark.grupo2.controller;
 import com.biopark.grupo2.model.Formulario;
+import com.biopark.grupo2.model.Usuario;
 import com.biopark.grupo2.repository.RepositoryFormulario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
+
 import java.util.List;
 
 @Controller
@@ -22,6 +26,13 @@ public class FormularioController {
         return modelAndView;
     }
 
+    @PostMapping("/lista_forms_base")
+    public RedirectView criarForm(@ModelAttribute ("formulario") Formulario formulario, RedirectAttributes attributes){
+        Formulario formularioSalvo = repositoryFormulario.save(formulario);
+        attributes.addFlashAttribute("condition", "cadastro-ok");
+        Long idFormulario = formularioSalvo.getId_formulario();
+        return new RedirectView("/detalhesDeFormulario/" + idFormulario);
+    }
     public List<Formulario> forms_base(){
         return repositoryFormulario.findAll();
     }
