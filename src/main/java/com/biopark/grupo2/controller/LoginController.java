2375dd1f6fb28b93fac64845c63630aecaba3eba
Matheus de Medeiros <137646466+MedeirosMatheus@users.noreply.github.com>
2024-05-17 19:26:24 -0300

    2 - ajustes controller
    - Controller reajustado para o modelo ModelAndView
    
    - Links reindexados.

--- src/main/java/com/biopark/grupo2/controller/LoginController.java
+++ src/main/java/com/biopark/grupo2/controller/LoginController.java
@@ -4,37 +4,37 @@ import com.biopark.grupo2.model.Usuario;
 import com.biopark.grupo2.repository.RepositoryLogin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Optional;
 
 @Controller
-@RequestMapping("/login")
 public class LoginController {
 
     @Autowired
     private RepositoryLogin usuarioRepository;
 
-    @GetMapping
-    public String showLoginPage(Model model) {
-        model.addAttribute("usuario", new Usuario());
-        return "login";
+    @GetMapping("/login")
+    public ModelAndView showLoginPage() {
+        ModelAndView mav = new ModelAndView("login");
+        mav.addObject("usuario", new Usuario());
+        return mav;
     }
 
-    @PostMapping
-    public String login(@RequestParam String email, @RequestParam String senha, Model model) {
+    @PostMapping("/login")
+    public ModelAndView login(@RequestParam String email, @RequestParam String senha) {
+        ModelAndView mav = new ModelAndView();
         Optional<Usuario> usuarioOpt = usuarioRepository.findByEmailAndSenha(email, senha);
         if (usuarioOpt.isPresent()) {
             // Login realizado, redireciona para a home
-            return "redirect:/lista_forms_base";
+            mav.setViewName("redirect:/lista_forms_base");
         } else {
             // Mensagem de erro
-            model.addAttribute("error", "Email ou senha inválidos.");
-            return "login";
+            mav.setViewName("redirect:/login");
         }
+        return mav;
     }
 }
