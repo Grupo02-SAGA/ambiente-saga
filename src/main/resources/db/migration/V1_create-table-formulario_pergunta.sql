b122a7815e95e1a096f5ed6312c0745e99b48541
heloshartmann <133721438+heloshartmann@users.noreply.github.com>
2024-05-20 21:42:30 -0300

    ajustando migrations

--- src/main/resources/db/migration/V1_create-table-formulario_pergunta.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE    Formulario_pergunta (
-                id_formulario INT,
-                id_pergunta INT,
-                PRIMARY KEY (id_formulario, id_pergunta),
-                FOREIGN KEY (id_formulario) REFERENCES Formulario(id_formulario),
-                FOREIGN KEY (id_pergunta) REFERENCES Pergunta(id_pergunta)
-                )
