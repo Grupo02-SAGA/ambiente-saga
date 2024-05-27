6ed2228f6eb959545cc7a5b39f8aeea5ec0b4a4b
Gustavo Dechotti <75210025+GusDechotti@users.noreply.github.com>
2024-05-26 21:50:23 -0300

    91-fix-ajuste-boto-ativoinativo (#92)

--- src/main/resources/db/migration/V1_create-table-formulario.sql
+++ src/main/resources/db/migration/V1_create-table-formulario.sql
@@ -2,5 +2,5 @@ CREATE TABLE Formulario(
     id_formulario INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     titulo VARCHAR(45),
     base BOOLEAN,
-    estado tinyint DEFAULT true
+    estado boolean DEFAULT true
 )
