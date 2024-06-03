ef6315d7e8eb3c325aed1b39fe894bab0b61174b
Gustavo Dechotti <75210025+GusDechotti@users.noreply.github.com>
2024-06-03 18:35:56 -0300

    Exibicao concluida

--- src/main/java/com/biopark/grupo2/controller/NovaRespController.java
+++ src/main/java/com/biopark/grupo2/controller/NovaRespController.java
@@ -1,24 +1,41 @@
 package com.biopark.grupo2.controller;
 
+import com.biopark.grupo2.model.Empresa;
 import com.biopark.grupo2.model.Formulario;
+import com.biopark.grupo2.repository.RepositoryEmpresa;
+import com.biopark.grupo2.repository.RepositoryFormulario;
 import com.biopark.grupo2.repository.RepositoryResposta;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+
 @Controller
 public class NovaRespController {
 
     @Autowired
-    private RepositoryResposta repositoryResposta;
+    private RepositoryFormulario repositoryFormulario;
+    @Autowired
+    private RepositoryEmpresa repositoryEmpresa;
 
     @GetMapping("/novaResposta")
     public ModelAndView paginaNovaResposta(){
         Formulario formulario = new Formulario();
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("novoRespForm");
+        modelAndView.addObject("forms",todosFormularios());
+        modelAndView.addObject("empresas", todasEmpresas());
         return modelAndView;
+    }
+
+    public List<Formulario> todosFormularios(){
+        return repositoryFormulario.findAll();
+    }
+
+    public List<Empresa> todasEmpresas(){
+        return repositoryEmpresa.findAll();
     }
 
 }
