101162f288a51d78843a87a469e4298321367264
devPisa <cesar.au563@gmail.com>
2024-06-14 05:11:31 -0300

    Feat: Implementação de listar perguntas do fomulario

--- src/main/java/com/biopark/grupo2/model/Formulario.java
+++ src/main/java/com/biopark/grupo2/model/Formulario.java
@@ -18,4 +18,7 @@ public class Formulario {
     private String titulo;
     private Boolean base;
     private Boolean estado;
+
+    @OneToMany(mappedBy = "id_formulario")
+    private List<Pergunta> perguntas;
 }
---
+++ src/main/resources/static/scripts/formulariosJS/executarFormulario.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelectorAll('.listaPergunta .tituloPergunta a').forEach(function(element) {
+        element.addEventListener('click', function(event) {
+            event.preventDefault();
+            let parent = element.closest('.listaPergunta');
+            let content = parent.querySelector('.alternativasPerguntas');
+            if (parent.classList.contains('ativo')) {
+                content.style.display = 'none';
+                parent.classList.remove('ativo');
+            } else {
+                content.style.display = 'block';
+                parent.classList.add('ativo');
+            }
+        });
+    });
+});
