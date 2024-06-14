93c44fc34b3b5a5abd7312f2f3a009da50cbfa8d
Kainak <125594835+Kainak@users.noreply.github.com>
2024-06-14 16:17:24 -0300

    89 tela lista de avaliaes em andamento   desenvolvimento (#103)
    * Paginação da página de lista de avaliações
    
    * filtros de busca pelo banco de dados

---
+++ src/main/java/com/biopark/grupo2/service/ServiceCertificado.java
@@ -0,0 +1,64 @@
+package com.biopark.grupo2.service;
+
+import com.biopark.grupo2.DTO.AvaliacaoDTO;
+import com.biopark.grupo2.repository.RepositoryCertificado;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Service
+public class ServiceCertificado {
+
+    @Autowired
+    private RepositoryCertificado repositoryCertificado;
+
+    public Page<AvaliacaoDTO> listarAvaliacoesPage(int page, int size, String filter, Integer status) {
+        Page<Object[]> resultados;
+        PageRequest pageable = PageRequest.of(page, size);
+
+        switch (filter) {
+            case "MaisRecente":
+                resultados = repositoryCertificado.findAvaliacoesMaisRecentes(pageable);
+                break;
+            case "MenosRecente":
+                resultados = repositoryCertificado.findAvaliacoesMenosRecentes(pageable);
+                break;
+            case "OrdemEmpresa":
+                resultados = repositoryCertificado.findAvaliacoesOrdemEmpresa(pageable);
+                break;
+            case "OrdemFuncionario":
+                resultados = repositoryCertificado.findAvaliacoesOrdemFuncionario(pageable);
+                break;
+            case "base1":
+                resultados = repositoryCertificado.findAvaliacoesByStatus(1, pageable);
+                break;
+            case "base2":
+                resultados = repositoryCertificado.findAvaliacoesByStatus(2, pageable);
+                break;
+            case "base3":
+                resultados = repositoryCertificado.findAvaliacoesByStatus(3, pageable);
+                break;
+            default:
+                resultados = repositoryCertificado.findAvaliacoesMaisRecentes(pageable);
+        }
+
+        return resultados.map(this::convertToDTO);
+    }
+
+    private AvaliacaoDTO convertToDTO(Object[] resultado) {
+        AvaliacaoDTO dto = new AvaliacaoDTO();
+        dto.setBase((Integer) resultado[0]);
+        dto.setUltimaMod((Date) resultado[1]);
+        dto.setFantasia((String) resultado[2]);
+        dto.setFantasia((String) resultado[3]);
+        dto.setUsuario((String) resultado[4]);
+        dto.setId_formulario((Long) resultado[5]);
+        dto.setId_certificado((Long) resultado[6]);
+        return dto;
+    }
+}
