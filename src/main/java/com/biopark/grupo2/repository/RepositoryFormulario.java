20a3988babefdc9f5be2b4c18dc4e7b80446504a
Gustavo Dechotti <75210025+GusDechotti@users.noreply.github.com>
2024-06-14 19:19:10 -0300

    82 desenvolvimento gerar um fomulrio para ser respondido (#101)
    * criacao html e controller
    
    * Alteração CSS
    
    * Exibicao concluida
    
    * post FUNCIONANDO
    
    * post FUNCIONANDO
    
    Ainda faltam as perguntas
    
    * backend pronto
    
    * Finalização com o front
    
    * Melhoria forms base
    
    Agora os formularios estão com o base da forma correta
    
    * Alteração css como pedido
    
    Fonte e tamanho da letra
    
    * update bug from main
    
    * arruamndo fonte
    
    ---------
    
    Co-authored-by: heloshartmann <133721438+heloshartmann@users.noreply.github.com>

--- src/main/java/com/biopark/grupo2/repository/RepositoryFormulario.java
+++ src/main/java/com/biopark/grupo2/repository/RepositoryFormulario.java
@@ -1,5 +1,14 @@
 package com.biopark.grupo2.repository;
 import com.biopark.grupo2.model.Formulario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
 public interface RepositoryFormulario extends JpaRepository<Formulario, Long> {
+    @Query("SELECT p FROM Formulario p WHERE p.id_formulario = (SELECT MAX(p2.id_formulario) FROM Formulario p2)")
+    Formulario consultaMaiorId();
+
+    @Query("SELECT id_formulario,titulo,base,estado FROM Formulario WHERE base = 0")
+    List<Formulario> consultaTodosOsBase();
 }
