0edc81d1d3d60d65e555368ace46785b7493fc8b
Kainak <natanfkainak@gmail.com>
2024-06-21 02:09:37 -0300

    Alterado filtro de busca por nome da empresa
    Agora ve direto do banco  para que não haja interferência da página

--- src/main/java/com/biopark/grupo2/repository/RepositoryCertificado.java
+++ src/main/java/com/biopark/grupo2/repository/RepositoryCertificado.java
@@ -10,6 +10,20 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface RepositoryCertificado extends JpaRepository<Certificado, Long> {
+
+    @Query(value = "SELECT f.base, r.ultimaMod AS data, f.titulo AS form, e.fantasia, u.nome AS usuario, f.id_formulario, c.id_certificado AS cert " +
+            "FROM certificado c " +
+            "JOIN empresa e ON e.id_empresa = c.id_empresa " +
+            "JOIN formulario f ON c.id_formulario = f.id_formulario " +
+            "JOIN resposta r ON r.id_formulario = f.id_formulario " +
+            "JOIN usuario u ON r.id_usuario = u.id_usuario " +
+            "WHERE e.fantasia LIKE %:empresa% " +
+            "AND (f.id_formulario, r.ultimaMod) IN (SELECT r2.id_formulario, MAX(r2.ultimaMod) FROM resposta r2 GROUP BY r2.id_formulario) " +
+            "ORDER BY e.fantasia ASC",
+            nativeQuery = true)
+    Page<Object[]> findAvaliacoesByEmpresaNome(@Param("empresa") String empresa, Pageable pageable);
+
+
     @Query(value = "SELECT f.base, r.ultimaMod AS data, f.titulo AS form, e.fantasia, u.nome AS usuario, f.id_formulario, c.id_certificado AS cert " +
             "FROM certificado c " +
             "JOIN empresa e ON e.id_empresa = c.id_empresa " +
@@ -63,4 +77,6 @@ public interface RepositoryCertificado extends JpaRepository<Certificado, Long> 
             "WHERE f.base = :status AND (f.id_formulario, r.ultimaMod) IN (SELECT r2.id_formulario, MAX(r2.ultimaMod) FROM resposta r2 GROUP BY r2.id_formulario)",
             nativeQuery = true)
     Page<Object[]> findAvaliacoesByStatus(@Param("status") int status, Pageable pageable);
+
+
 }
