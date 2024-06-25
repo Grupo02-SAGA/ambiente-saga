0bd2f895e93e27594ff333de45b560a469d6ef7a
devPisa <cesar.au563@gmail.com>
2024-06-25 01:12:14 -0300

    Feat: Implementação CSS e JS dos input de resposta

--- src/main/resources/static/scripts/formulariosJS/executarFormulario.js
+++ src/main/resources/static/scripts/formulariosJS/executarFormulario.js
@@ -1,16 +1,40 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.querySelectorAll('.listaPergunta .tituloPergunta a').forEach(function(element) {
-        element.addEventListener('click', function(event) {
+document.addEventListener('DOMContentLoaded', function() {
+    const perguntas = document.querySelectorAll('.listaPergunta');
+
+    perguntas.forEach(pergunta => {
+        const titulo = pergunta.querySelector('.tituloPergunta a');
+        const container = pergunta.querySelector('.perguntaContainer');
+        const tituloTexto = pergunta.querySelector('.tituloPergunta h3');
+        const tituloOriginal = tituloTexto.textContent;
+
+        titulo.addEventListener('click', function(event) {
             event.preventDefault();
-            let parent = element.closest('.listaPergunta');
-            let content = parent.querySelector('.alternativasPerguntas');
-            if (parent.classList.contains('ativo')) {
-                content.style.display = 'none';
-                parent.classList.remove('ativo');
+            pergunta.classList.toggle('expandir');
+            container.style.display = container.style.display === 'flex' ? 'none' : 'flex';
+
+            const checkboxes = container.querySelectorAll('input[type="checkbox"]');
+            let algumMarcado = Array.from(checkboxes).some(checkbox => checkbox.checked);
+
+            if (algumMarcado && !pergunta.classList.contains('expandir')) {
+                tituloTexto.textContent = 'RESPONDIDO';
+                tituloTexto.style.color = 'green';
             } else {
-                content.style.display = 'flex';
-                parent.classList.add('ativo');
+                tituloTexto.textContent = tituloOriginal;
+                tituloTexto.style.color = '';
             }
+        });
+
+        const checkboxes = container.querySelectorAll('input[type="checkbox"]');
+        checkboxes.forEach(checkbox => {
+            checkbox.addEventListener('change', function() {
+                if (this.checked) {
+                    checkboxes.forEach(box => {
+                        if (box !== this) {
+                            box.checked = false;
+                        }
+                    });
+                }
+            });
         });
     });
 });
