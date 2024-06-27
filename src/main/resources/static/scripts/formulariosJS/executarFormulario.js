4912490504715bbf68db6a040f22ea46486db123
devPisa <cesar.au563@gmail.com>
2024-06-27 07:14:49 -0300

    Feat: Implementação de popup issue #123

--- src/main/resources/static/scripts/formulariosJS/executarFormulario.js
+++ src/main/resources/static/scripts/formulariosJS/executarFormulario.js
@@ -100,4 +100,35 @@ document.addEventListener('DOMContentLoaded', function() {
 
 });
 
+//Iniciando popup
+const popupCancelar = document.getElementById('cancelarResposta');
+const fade_fundo = document.getElementById("fade_cancelar");
+const popup = document.getElementById("popup");
+const button_sim_popup = document.getElementById("confirmar_fragmento");
+const button_nao_popup = document.getElementById("cancelar_fragmento");
+
+const togglePopup = () => {
+    [popup, fade_fundo].forEach((el) => el.classList.toggle("hide"));
+};
+
+popupCancelar.addEventListener("click", () => {
+    togglePopup();
+});
+
+const redirecionar_pagina = () => {
+    return location.href = "/executarAvaliacao";
+};
+
+if (button_sim_popup) {
+    button_sim_popup.addEventListener("click", () => {
+        redirecionar_pagina();
+    });
+}
+
+if (button_nao_popup) {
+    button_nao_popup.addEventListener("click", () => {
+        togglePopup();
+    });
+}
+
 
